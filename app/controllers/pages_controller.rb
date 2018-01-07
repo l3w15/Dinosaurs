04ff6104c14ba48c43b94a3f951509c793b17ba3
kotpsot <zygmunt.justyna@gmail.com>
2018-01-07 21:13:31 +0000

    html template of website

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home # tylko na serwerze
+  end
+
+  # def create
+  #   @dinotable = Dinotable.new(dino_params)
+  #   if @dinotable.save
+  #     redirect_to '/dinosaurs'
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+end
