2587d928a153c7dfd679d7fff7d3713cbe5573af
kotpsot <zygmunt.justyna@gmail.com>
2018-01-08 19:12:37 +0000

    add all sections to the website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,22 @@ class PagesController < ApplicationController
   def home
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+  	@place = Place.new(place_params)
+  	if @place.save
+    	redirect_to '/home'
+  	else
+    	render 'new'
+  	end
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address, :image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pages#home'
+  get '/home' => 'pages#home'
+  get '/home/new' => 'pages#new'
+  post 'home' => 'pages#create'
+  
 end
